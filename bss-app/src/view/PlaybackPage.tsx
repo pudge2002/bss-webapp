b0f02b28ce28aa59a98aa3db2ca7a257f92e03aa
s3x <105816936+pudge2002@users.noreply.github.com>
2025-03-12 20:57:16 +0300

    vid

--- bss-app/src/view/PlaybackPage.tsx
+++ bss-app/src/view/PlaybackPage.tsx
@@ -3,7 +3,8 @@ import "../styles/playbackPageStyle.css"
 const PlaypackPage: React.FC = () => (
     <div className="content-cameras">
         <div className="cameras"><h1>камеры</h1></div>
-        <div className="showingCam"><h1>выбранные камеры</h1></div>
+        <div className="showingCam"><h1>выбранные камеры</h1>
+        <video className="output-video" width={1200} height={700}></video></div>
     </div>
 );
 
